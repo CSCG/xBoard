d727ae983b30e29bc0eff6919f3407035c094263
William <william@xondesigns.com>
2014-10-20 12:24:24 -0500

    New Board System

--- assets/js/recaptcha.js
+++
@@ -1,7 +0,0 @@
-Recaptcha.create("6Lex5OQSAAAAAOWUPB8TcWQPAM-jr_bOAQXBaNvQ",
-	"recaptcha",
-	{
-		theme: "clean",
-		callback: Recaptcha.focus_response_field
-	}
-);
--- assets/js/xboard.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('.unixtime').each(function(){
-		var timestamp = $(this).attr("data-timestamp");
-		var date = new Date(timestamp*1000);
-		$(this).html(date.toLocaleTimeString() + " " + date.toLocaleDateString());
-	});
-	var user = JSON.parse($.base64.decode($.cookie('session')));
-	$("#name").val(user.name);
-});
